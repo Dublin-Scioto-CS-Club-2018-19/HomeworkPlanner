9d7dea3679b2eb332d412177170e29a688dc7fc3
MichaelDSHS <43283187+MichaelDSHS@users.noreply.github.com>
2018-09-27 19:54:21 -0400

    Subject EditText to Description and dates
    -Day and month spinners added

--- app/src/main/java/com/DSHS/HwPlanner/Main/NewAssignmentActivity.java
+++ app/src/main/java/com/DSHS/HwPlanner/Main/NewAssignmentActivity.java
@@ -21,8 +21,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Activity for entering a word.
@@ -32,16 +34,46 @@ public class NewAssignmentActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.assignmentlistsql.REPLY";
 
+    // add more for entering assignment details
     private  EditText mEditAssignmentView;
-    private EditText mEditAssignmentSubject;
+    private EditText mEditAssignmentDescription;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_assignment);
-        mEditAssignmentView = findViewById(R.id.edit_assignment);
-        mEditAssignmentSubject = findViewById(R.id.edit_assignment_subject);
 
+        // add more for entering assignment details
+        mEditAssignmentView = findViewById(R.id.edit_assignment);
+        mEditAssignmentDescription = findViewById(R.id.edit_assignment_description);
+
+        // spinners
+
+        // month spinner
+        String[] arrayMonths = new String[] {
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
+        };
+
+        final Spinner spinner_month = findViewById(R.id.spinner_month_assignment);
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arrayMonths);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_month.setAdapter(monthAdapter);
+
+        // day spinner
+        String[] arrayDays = new String[] {
+                "1", "2",  "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+                "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                "30", "31"
+        };
+        final Spinner spinner_day = findViewById(R.id.spinner_day_assignment);
+        ArrayAdapter<String> dayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arrayDays);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_day.setAdapter(dayAdapter);
+
+
+        // save button
         final Button button = findViewById(R.id.button_save_assignment);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -50,15 +82,28 @@ public class NewAssignmentActivity extends AppCompatActivity {
                     setResult(RESULT_CANCELED, replyIntent);
                 } else {
                     String assignment = mEditAssignmentView.getText().toString();
-                    if (!TextUtils.isEmpty(mEditAssignmentSubject.getText())) {
-                        assignment += "\n" + mEditAssignmentSubject.getText().toString();
+                    // description
+                    if (!TextUtils.isEmpty(mEditAssignmentDescription.getText())) {
+                        assignment += "\n" + mEditAssignmentDescription.getText().toString();
                     }
+                    // date block
+                    assignment += " ";
+                    // day
+                    if (!TextUtils.isEmpty(spinner_day.getSelectedItem().toString())){
+                        assignment += "Day:" + spinner_day.getSelectedItem().toString() + " ";
+                    }
+                    // month
+                    if (!TextUtils.isEmpty(spinner_month.getSelectedItem().toString())){
+                        assignment += "Month:" + spinner_month.getSelectedItem().toString() + " ";
+                    }
+
                     replyIntent.putExtra(EXTRA_REPLY, assignment);
                     setResult(RESULT_OK, replyIntent);
                 }
                 finish();
             }
         });
+
     }
 }
 
